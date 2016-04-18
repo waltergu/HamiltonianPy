71ad555d4961c86612efddbede30f54270acb059
waltergu <waltergu1989@gmail.com>
2016-04-18 23:49:16 +0800

    For synchronization.

---
+++ setup.py
@@ -0,0 +1,11 @@
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config=Configuration('HamiltonianPP',parent_package,top_path,version='0.0.0',author='waltergu',author_email='waltergu@126.com')
+    config.add_subpackage('Core')
+    #config.add_subpackage('DataBase')
+    config.make_config_py()
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(configuration=configuration)
