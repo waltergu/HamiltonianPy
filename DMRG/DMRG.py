f93a798c92f10fc87594707d4a630fceace80bb4
waltergu <waltergu1989@gmail.com>
2016-06-07 21:56:40 +0800

    For synchronization.

--- DMRG/DMRG.py
+++ DMRG/DMRG.py
@@ -4,3 +4,21 @@ DMRG.
 
 from ..Math.Tensor import *
 from ..Math.MPS import *
+from ..Basics import *
+
+class Block(object):
+    '''
+    '''
+    def __init__(self,length,basis,H):
+        self.length=length
+        self.basis=basis
+        self.H=H
+
+class IDMRG(engine):
+    '''
+    '''
+    def __init__(self,name,lattice,config,term,**karg):
+        self.name=name
+        self.lattice=lattice
+        self.config=config
+        
