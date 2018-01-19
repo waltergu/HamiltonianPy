531cc438758804c999835f5a929ccf236d3287b0
waltergu <waltergu1989@gmail.com>
2018-01-19 13:49:41 +0800

    Doc updated.

--- Doc/source/conf.py
+++ Doc/source/conf.py
@@ -17,7 +17,7 @@ extensions=[
 # Napoleon settings
 napoleon_google_docstring=False
 napoleon_numpy_docstring=True
-napoleon_include_init_with_doc=True
+napoleon_include_init_with_doc=False
 napoleon_include_private_with_doc=True
 napoleon_include_special_with_doc=True
 napoleon_use_admonition_for_examples=False
--- __init__.py
+++ __init__.py
@@ -3,19 +3,24 @@
 Introduction
 ############
 
-This package provides a unified framework to generate the operators of electron and spin systems.
-Based on this, it implements several algorithms, including:
+In this package, we provide a unified framework to make a generic quantum lattice system programmable. Two central concepts are proposed to acheive this:
 
-* tight binding approximation (TBA) and Bogoliubov de Gennes (BdG) equations for fermionic systems,
-* exact diagonalization (ED) for electron systems and spin systems,
-* cluster perturbation theory (CPT) and variational cluster approach (VCA) for fermionic systems,
-* density matrix renormalization group (DMRG) for electron systems and spin systems,
-* spin wave theory for flat band ferromagnets.
+* unit-cell description ansatz (UCDA), and
+* engine-app framework (EAF).
+
+UCDA incoporates the geometry of the latttice, the internal degrees of freedom of the Hilbert space and the terms of the Hamiltonian as a whole, and generates all the needed operators of the system. EAF handles the interactions between the core algotithms (engine) and the user-defined tasks (app) as well as the logging of the program and the recording of the results, etc. Fundamental classes and functions are offered in the subpackage `Basics` as the APIs. Based on this, several algorithms are implemented, including:
+
+* subpackage `FreeSystem`: tight binding approximation (TBA) and Bogoliubov de Gennes (BdG) equations for fermionic systems,
+* subpackage `ED`: exact diagonalization (ED) for electron and spin systems,
+* subpackage `VCA`: cluster pertubation theory (CPT) and variational cluster approach (VCA) for fermionic systems,
+* subpackage `DMRG`: density matrix renormalization group (DMRG) for electron and spin systems,
+* subpackage `FBFM`: spin wave theory for flat band ferromagnets (FBFM).
 
 It works with python 2.7, and requires several packages:
 
 * numpy latest version
 * scipy latest version
+* matplotlib latest version
 * mpi4py
 
 For source code, please visit https://github.com/waltergu/HamiltonianPy.
@@ -38,9 +43,9 @@ SUBPACKAGE        DESCRIPTION
 `Management`      Project construction and maintenance
 ===============   ==========================================================================
 
-#################
+########
 Contents
-#################
+########
 
 .. toctree::
    :numbered:
